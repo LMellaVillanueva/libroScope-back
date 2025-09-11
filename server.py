8a72d04487458d560b5b1f9023ff2d95400d1e43
LMellaVillanueva <mellalucas.v@gmail.com>
2025-09-11 20:07:58 -0300

    cloudinary

--- server.py
+++ server.py
@@ -4,12 +4,13 @@ from app import app
 import os
 import cloudinary
 
-cloudinary.config(
-    cloud_name = os.getenv('CLOUDINARY_CLOUD_NAME'),
-    api_key = os.getenv('CLOUDINARY_API_KEY'),
-    api_secret = os.getenv('CLOUDINARY_API_SECRET'),
-    secure = True
-)
+def configure_cloudinary():
+    cloudinary.config(
+        cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+        api_key=os.getenv("CLOUDINARY_API_KEY"),
+        api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+        secure=True
+    )
 
 def create_app():
     app = Flask(__name__)
